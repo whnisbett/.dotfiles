8f5744904e3def70cef8a5d77d2513ef957c1d0d
Billy Nisbett <whnisbett@yelp.com>
2023-05-18 03:58:49 -0700

    add awesome_vimrc to install list

--- establish_links.sh
+++ establish_links.sh
@@ -7,7 +7,14 @@ done
 
 # INSTALL DEPENDENCIES
 cd ~
+# OH-MY-ZSH
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# AWESOME_VIMRC
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+# POWERLEVEL10K
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+# THEFUCK
 brew install thefuck
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# ZSH-AUTOSUGGESTION
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
