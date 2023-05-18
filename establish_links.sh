1776b343c8861387a1b718fb7b459c2492d18cda
Billy Nisbett <whnisbett@yelp.com>
2023-05-18 05:38:04 -0700

    add pyenv/direnv installations

--- establish_links.sh
+++ establish_links.sh
@@ -1,12 +1,5 @@
-# ESTABLISH LINKS IN HOME DIR
-cd ./dotfiles
-for file in *; do
-    echo "$(pwd)/$file"
-    ln -sf $(pwd)/"$file" ~/."$file"
-done
-
+#!/bin/zsh
 # INSTALL DEPENDENCIES
-cd ~
 # OH-MY-ZSH
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # AWESOME_VIMRC
@@ -14,7 +7,23 @@ git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 # POWERLEVEL10K
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-# THEFUCK
-brew install thefuck
 # ZSH-AUTOSUGGESTION
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# DIRENV
+curl -sfL https://direnv.net/install.sh
+# PYENV
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+# PYENV VIRTUALENV
+git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # THEFUCK
+    brew install thefuck
+fi
+
+# ESTABLISH LINKS IN HOME DIR
+cd ./dotfiles
+for file in *; do
+    echo "$(pwd)/$file"
+    ln -sf $(pwd)/"$file" ~/."$file"
+done
+
